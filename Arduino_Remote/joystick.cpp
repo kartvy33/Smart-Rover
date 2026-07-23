3e9e0b61983d7aced19391bc1b393b032166ee06
kartvy33 <kartvyrathod33@gmail.com>
2026-07-23 21:20:08 +0530

    Create joystick.cpp

---
+++ Arduino_Remote/joystick.cpp
@@ -0,0 +1,24 @@
+#include "joystick.h"
+
+void joystickBegin()
+{
+    pinMode(JOY_SW_PIN, INPUT_PULLUP);
+}
+
+void readJoystick(ControlPacket &packet)
+{
+    int x = analogRead(JOY_X_PIN);
+    int y = analogRead(JOY_Y_PIN);
+
+    // Deadband
+    if (abs(x - JOY_CENTER) < JOY_DEADBAND)
+        x = JOY_CENTER;
+
+    if (abs(y - JOY_CENTER) < JOY_DEADBAND)
+        y = JOY_CENTER;
+
+    packet.joyX = x;
+    packet.joyY = y;
+
+    packet.joyButton = !digitalRead(JOY_SW_PIN);
+}
