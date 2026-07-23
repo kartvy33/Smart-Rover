96e36d351f932875971341a7415acd786b948b83
kartvy33 <kartvyrathod33@gmail.com>
2026-07-23 21:27:40 +0530

    Create Remote.ino

---
+++ Arduino_Remote/Remote.ino
@@ -0,0 +1,69 @@
+/*
+====================================================
+      SMART ROVER REMOTE CONTROLLER
+====================================================
+
+Board : Arduino UNO
+
+Joystick
+Buttons
+NRF24L01 PA+LNA
+
+Author : Smart-Rover Project
+
+====================================================
+*/
+
+#include "config.h"
+#include "radio.h"
+#include "joystick.h"
+#include "buttons.h"
+#include "controls.h"
+
+ControlPacket packet;
+
+void setup()
+{
+    Serial.begin(115200);
+
+    joystickBegin();
+
+    buttonsBegin();
+
+    radioBegin();
+
+    Serial.println();
+    Serial.println("==================================");
+    Serial.println(" Smart Rover Remote Started");
+    Serial.println("==================================");
+}
+
+void loop()
+{
+    updateControls(packet);
+
+    sendPacket(packet);
+
+    Serial.print("X:");
+    Serial.print(packet.joyX);
+
+    Serial.print("  Y:");
+    Serial.print(packet.joyY);
+
+    Serial.print("  SW:");
+    Serial.print(packet.joyButton);
+
+    Serial.print("  B1:");
+    Serial.print(packet.button1);
+
+    Serial.print("  B2:");
+    Serial.print(packet.button2);
+
+    Serial.print("  B3:");
+    Serial.print(packet.button3);
+
+    Serial.print("  B4:");
+    Serial.println(packet.button4);
+
+    delay(20);
+}
