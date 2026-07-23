d774ea66a810b1a881d2b3ffa996d00ff2f34492
kartvy33 <kartvyrathod33@gmail.com>
2026-07-23 22:10:07 +0530

    Create config.h

---
+++ ESP32_Rover/config.h
@@ -0,0 +1,71 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include <Arduino.h>
+
+/*=====================================================
+                SMART ROVER CONFIG
+======================================================*/
+
+/**************** NRF24 ****************/
+#define NRF_CE      4
+#define NRF_CSN     5
+
+/**************** L298N ****************/
+#define MOTOR_ENA   25
+#define MOTOR_IN1   26
+#define MOTOR_IN2   27
+#define MOTOR_IN3   14
+#define MOTOR_IN4   13
+#define MOTOR_ENB   33
+
+/**************** Ultrasonic ***********/
+#define TRIG_PIN    32
+#define ECHO_PIN    35
+
+/**************** IR Sensors ***********/
+#define IR_LEFT     34
+#define IR_RIGHT    2
+
+/**************** GPS ******************/
+#define GPS_RX      16
+#define GPS_TX      17
+
+/**************** LCD ******************/
+#define LCD_SDA     21
+#define LCD_SCL     22
+
+/**************** Speaker ***************/
+#define SPEAKER_PIN 12
+
+/**************** Battery ***************/
+#define BATTERY_PIN 15
+
+/**************** Battery Divider ********
+100kΩ
+ |
+ +---- GPIO15
+ |
+47kΩ
+ |
+Battery -
+****************************************/
+
+/**************** Voltage ***************/
+
+#define BATTERY_MAX 8.40
+#define BATTERY_MIN 6.40
+
+/**************** PWM *******************/
+
+#define PWM_FREQ        1000
+#define PWM_RESOLUTION  8
+
+#define PWM_CH_LEFT     0
+#define PWM_CH_RIGHT    1
+
+/**************** Rover *****************/
+
+#define MAX_SPEED       255
+
+#endif
