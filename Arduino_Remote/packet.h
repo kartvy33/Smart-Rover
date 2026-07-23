63646a82797df18dcfc5e2b5bd5bb5a6aaf094f8
kartvy33 <kartvyrathod33@gmail.com>
2026-07-23 22:28:06 +0530

    Create packet.h

---
+++ Arduino_Remote/packet.h
@@ -0,0 +1,27 @@
+#ifndef PACKET_H
+#define PACKET_H
+
+#include <Arduino.h>
+
+struct ControlPacket
+{
+    uint16_t startByte;
+
+    int16_t joyX;
+    int16_t joyY;
+
+    bool joyButton;
+
+    bool button1;
+    bool button2;
+    bool button3;
+    bool button4;
+
+    uint16_t packetNumber;
+
+    uint16_t checksum;
+};
+
+uint16_t calculateChecksum(ControlPacket &packet);
+
+#endif
