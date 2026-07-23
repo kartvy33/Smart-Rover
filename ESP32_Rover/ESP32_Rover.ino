d79ff270d213cfe6cc5d7f4047dac5d70b3a1a6b
kartvy33 <kartvyrathod33@gmail.com>
2026-07-23 22:11:02 +0530

    Create ESP32_Rover.ino

---
+++ ESP32_Rover/ESP32_Rover.ino
@@ -0,0 +1,27 @@
+/*
+=========================================
+AI POWERED SMART ROVER
+
+Main Controller
+
+ESP32 DevKit V1
+=========================================
+*/
+
+#include "config.h"
+
+void setup()
+{
+    Serial.begin(115200);
+
+    Serial.println();
+    Serial.println("===========================");
+    Serial.println("SMART ROVER");
+    Serial.println("ESP32 MAIN CONTROLLER");
+    Serial.println("===========================");
+}
+
+void loop()
+{
+
+}
