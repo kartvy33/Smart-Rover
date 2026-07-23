2dd81a87844f19bbca4389f95b325a507f5e2bde
kartvy33 <kartvyrathod33@gmail.com>
2026-07-23 22:20:13 +0530

    Update ESP32_Rover.ino

--- ESP32_Rover/ESP32_Rover.ino
+++ ESP32_Rover/ESP32_Rover.ino
@@ -1,27 +1,18 @@
-/*
-=========================================
-AI POWERED SMART ROVER
-
-Main Controller
-
-ESP32 DevKit V1
-=========================================
-*/
-
 #include "config.h"
+#include "motors.h"
 
 void setup()
 {
     Serial.begin(115200);
 
+    motorsBegin();
+
     Serial.println();
-    Serial.println("===========================");
     Serial.println("SMART ROVER");
-    Serial.println("ESP32 MAIN CONTROLLER");
-    Serial.println("===========================");
+    Serial.println("Motor Driver Ready");
 }
 
 void loop()
 {
-
+    // Waiting for NRF24 commands
 }
