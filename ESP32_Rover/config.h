c4f7014ee50278e377f3177ae2e73b9bf541b454
kartvy33 <kartvyrathod33@gmail.com>
2026-07-24 19:59:50 +0530

    Update config.h

--- ESP32_Rover/config.h
+++ ESP32_Rover/config.h
@@ -1,76 +1,109 @@
 #ifndef CONFIG_H
 #define CONFIG_H
-#define OBSTACLE_DISTANCE_CM 20
-#define CLIFF_DETECTED LOW
-#define LOW_BATTERY_PERCENT 15
 
 #include <Arduino.h>
+
+/* ===========================
+   Firmware
+   =========================== */
+
+#define FIRMWARE_VERSION "1.0"
+
+/* ===========================
+   Debug
+   =========================== */
+
+#define DEBUG_MODE 1
+
+#if DEBUG_MODE
+#define DEBUG_PRINT(x) Serial.print(x)
+#define DEBUG_PRINTLN(x) Serial.println(x)
+#else
+#define DEBUG_PRINT(x)
+#define DEBUG_PRINTLN(x)
+#endif
+
+/* ===========================
+   Motor Driver (TB6612FNG)
+   =========================== */
+
+#define MOTOR_IN1 26
+#define MOTOR_IN2 27
+#define MOTOR_IN3 14
+#define MOTOR_IN4 12
+
+#define MOTOR_PWM_LEFT 25
+#define MOTOR_PWM_RIGHT 33
+
+#define MOTOR_PWM_FREQ 1000
+#define MOTOR_PWM_RES 8
+
+#define MAX_SPEED 255
+
+/* ===========================
+   NRF24
+   =========================== */
+
+#define RADIO_CE 4
+#define RADIO_CSN 5
+
 const uint8_t RADIO_ADDRESS[6] = "ROVER";
 
-/*=====================================================
-                SMART ROVER CONFIG
-======================================================*/
+/* ===========================
+   GPS
+   =========================== */
 
-/**************** NRF24 ****************/
-#define NRF_CE      4
-#define NRF_CSN     5
+#define GPS_RX 16
+#define GPS_TX 17
 
-/**************** L298N ****************/
-#define MOTOR_ENA   25
-#define MOTOR_IN1   26
-#define MOTOR_IN2   27
-#define MOTOR_IN3   14
-#define MOTOR_IN4   13
-#define MOTOR_ENB   33
+/* ===========================
+   LCD
+   =========================== */
 
-/**************** Ultrasonic ***********/
-#define TRIG_PIN    32
-#define ECHO_PIN    35
+#define LCD_SDA 21
+#define LCD_SCL 22
 
-/**************** IR Sensors ***********/
-#define IR_LEFT     34
-#define IR_RIGHT    2
+/* ===========================
+   Battery
+   =========================== */
 
-/**************** GPS ******************/
-#define GPS_RX      16
-#define GPS_TX      17
-
-/**************** LCD ******************/
-#define LCD_SDA     21
-#define LCD_SCL     22
-
-/**************** Speaker ***************/
-#define SPEAKER_PIN 12
-
-/**************** Battery ***************/
 #define BATTERY_PIN 15
 
-/**************** Battery Divider ********
-100kΩ
- |
- +---- GPIO15
- |
-47kΩ
- |
-Battery -
-****************************************/
-
-/**************** Voltage ***************/
-
 #define BATTERY_MAX 8.40
-#define BATTERY_MIN 6.40
+#define BATTERY_MIN 6.00
 
-/**************** PWM *******************/
+#define LOW_BATTERY_PERCENT 15
 
-#define PWM_FREQ        1000
-#define PWM_RESOLUTION  8
+/* ===========================
+   Ultrasonic
+   =========================== */
 
-#define PWM_CH_LEFT     0
-#define PWM_CH_RIGHT    1
+#define TRIG_PIN 32
+#define ECHO_PIN 35
 
-/**************** Rover *****************/
+#define OBSTACLE_DISTANCE_CM 20
 
-#define MAX_SPEED       255
-#define RADIO_TIMEOUT 500
+/* ===========================
+   IR Sensors
+   =========================== */
+
+#define IR_LEFT 34
+#define IR_RIGHT 2
+
+#define CLIFF_DETECTED LOW
+
+/* ===========================
+   Speaker
+   =========================== */
+
+#define SPEAKER_PIN 13
+#define SPEAKER_CHANNEL 7
+#define SPEAKER_RESOLUTION 8
+
+/* ===========================
+   Control
+   =========================== */
+
+#define DEADZONE 100
 
 #endif
