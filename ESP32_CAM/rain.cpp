604a944ee1a39e06288d024f358aedfd9e7a0a54
kartvy33 <kartvyrathod33@gmail.com>
2026-07-24 21:36:09 +0530

    Create rain.cpp

---
+++ ESP32_CAM/rain.cpp
@@ -0,0 +1,32 @@
+#include "rain.h"
+
+#define RAIN_PIN 14
+
+static int rainValue=0;
+
+static bool raining=false;
+
+void rainBegin()
+{
+    pinMode(RAIN_PIN,INPUT);
+}
+
+void rainUpdate()
+{
+    rainValue=analogRead(RAIN_PIN);
+
+    if(rainValue<1800)
+        raining=true;
+    else
+        raining=false;
+}
+
+bool isRaining()
+{
+    return raining;
+}
+
+int getRainValue()
+{
+    return rainValue;
+}
