58c43bbbad13c46fd73b306fbf322a792b5766c5
kartvy33 <kartvyrathod33@gmail.com>
2026-07-24 21:11:15 +0530

    Create ESP32_CAM.ino

---
+++ ESP32_CAM/ESP32_CAM.ino
@@ -0,0 +1,34 @@
+#include "config.h"
+
+#include "camera.h"
+#include "wifi_manager.h"
+
+void setup()
+{
+    Serial.begin(115200);
+
+    Serial.println();
+    Serial.println("========================");
+    Serial.println(" SMART ROVER CAMERA ");
+    Serial.println("========================");
+
+    wifiBegin();
+
+    if(cameraBegin())
+    {
+        Serial.println("Camera Initialized");
+    }
+    else
+    {
+        Serial.println("Camera Failed");
+
+        while(true)
+        {
+            delay(1000);
+        }
+    }
+}
+
+void loop()
+{
+}
