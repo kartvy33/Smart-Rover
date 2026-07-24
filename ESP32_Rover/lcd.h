a84e80fdd227b5e73e01dfd1d51aa24104ed4325
kartvy33 <kartvyrathod33@gmail.com>
2026-07-24 20:36:29 +0530

    Update lcd.h

--- ESP32_Rover/lcd.h
+++ ESP32_Rover/lcd.h
@@ -2,16 +2,17 @@
 #define LCD_H
 
 #include <Arduino.h>
-#include <Wire.h>
-#include <LiquidCrystal_I2C.h>
 
 void lcdBegin();
+
+void lcdUpdate();
 
 void lcdShowBoot();
 
 void lcdShowStatus(
     int battery,
     bool radio,
-    int satellites);
+    int satellites
+);
 
 #endif
