82f9c515918d759fe4c29b1afa6cd9ed56501eda
kartvy33 <kartvyrathod33@gmail.com>
2026-07-24 20:37:09 +0530

    Update lcd.cpp

--- ESP32_Rover/lcd.cpp
+++ ESP32_Rover/lcd.cpp
@@ -1,11 +1,28 @@
 #include "lcd.h"
 
-LiquidCrystal_I2C lcd(0x27,16,2);
+#include <Wire.h>
+#include <hd44780.h>
+#include <hd44780ioClass/hd44780_I2Cexp.h>
+
+hd44780_I2Cexp lcd;
+
+static uint8_t page = 0;
+
+static unsigned long pageTimer = 0;
 
 void lcdBegin()
 {
-    lcd.init();
+    lcd.begin(16,2);
+
+    lcd.clear();
+
     lcd.backlight();
+
+    lcd.print("LCD Ready");
+
+    delay(1000);
+
+    lcd.clear();
 }
 
 void lcdShowBoot()
@@ -13,10 +30,10 @@ void lcdShowBoot()
     lcd.clear();
 
     lcd.setCursor(0,0);
-    lcd.print(" SMART ROVER ");
+    lcd.print("SMART ROVER");
 
     lcd.setCursor(0,1);
-    lcd.print(" Initializing");
+    lcd.print("Firmware 1.0");
 }
 
 void lcdShowStatus(
@@ -24,21 +41,67 @@ void lcdShowStatus(
     bool radio,
     int satellites)
 {
-    lcd.clear();
+    if(millis()-pageTimer>3000)
+    {
+        page++;
 
-    lcd.setCursor(0,0);
-    lcd.print("BAT:");
-    lcd.print(battery);
-    lcd.print("%");
+        if(page>2)
+            page=0;
 
-    lcd.setCursor(10,0);
+        pageTimer=millis();
 
-    if(radio)
-        lcd.print("RF OK");
-    else
-        lcd.print("RF XX");
+        lcd.clear();
+    }
 
-    lcd.setCursor(0,1);
-    lcd.print("SAT:");
-    lcd.print(satellites);
+    switch(page)
+    {
+        case 0:
+
+            lcd.setCursor(0,0);
+            lcd.print("Battery:");
+
+            lcd.setCursor(10,0);
+            lcd.print(battery);
+            lcd.print("%");
+
+            lcd.setCursor(0,1);
+
+            if(radio)
+                lcd.print("Radio: OK");
+            else
+                lcd.print("Radio: LOST");
+
+        break;
+
+        case 1:
+
+            lcd.setCursor(0,0);
+            lcd.print("GPS Sats:");
+
+            lcd.setCursor(10,0);
+            lcd.print(satellites);
+
+            lcd.setCursor(0,1);
+
+            if(satellites>=4)
+                lcd.print("GPS FIX");
+            else
+                lcd.print("Searching");
+
+        break;
+
+        case 2:
+
+            lcd.setCursor(0,0);
+            lcd.print("Smart Rover");
+
+            lcd.setCursor(0,1);
+            lcd.print("Ready");
+
+        break;
+    }
+}
+
+void lcdUpdate()
+{
 }
