618242f55c581ebad335cc0f21d1ed0411bce32b
kartvy33 <kartvyrathod33@gmail.com>
2026-07-24 21:14:33 +0530

    Create servo.cpp

---
+++ ESP32_CAM/servo.cpp
@@ -0,0 +1,52 @@
+#include "servo.h"
+
+#include <ESP32Servo.h>
+
+#define SERVO_X_PIN 12
+#define SERVO_Y_PIN 13
+
+Servo servoX;
+Servo servoY;
+
+static int currentX = 90;
+static int currentY = 90;
+
+void servoBegin()
+{
+    servoX.setPeriodHertz(50);
+    servoY.setPeriodHertz(50);
+
+    servoX.attach(SERVO_X_PIN);
+    servoY.attach(SERVO_Y_PIN);
+
+    servoX.write(currentX);
+    servoY.write(currentY);
+}
+
+void setServoX(int angle)
+{
+    angle = constrain(angle, 0, 180);
+
+    currentX = angle;
+
+    servoX.write(angle);
+}
+
+void setServoY(int angle)
+{
+    angle = constrain(angle, 0, 180);
+
+    currentY = angle;
+
+    servoY.write(angle);
+}
+
+int getServoX()
+{
+    return currentX;
+}
+
+int getServoY()
+{
+    return currentY;
+}
