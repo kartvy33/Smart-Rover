b7006b66165dad8c710463d568ac38c1976bfe4d
kartvy33 <kartvyrathod33@gmail.com>
2026-07-24 21:58:38 +0530

    Create status.h

---
+++ ESP32_CAM/status.h
@@ -0,0 +1,16 @@
+#ifndef STATUS_H
+#define STATUS_H
+
+extern float temperature;
+
+extern float humidity;
+
+extern bool raining;
+
+extern int battery;
+
+extern bool gps;
+
+extern bool radioConnected;
+
+#endif
