9c61025a0e7a913016f046e6dca0e8b52d5b7a81
kartvy33 <kartvyrathod33@gmail.com>
2026-07-24 19:43:38 +0530

    Create speaker.h

---
+++ ESP32_Rover/speaker.h
@@ -0,0 +1,18 @@
+#ifndef SPEAKER_H
+#define SPEAKER_H
+
+#include <Arduino.h>
+
+void speakerBegin();
+
+void beepShort();
+
+void beepLong();
+
+void obstacleAlarm();
+
+void cliffAlarm();
+
+void batteryAlarm();
+
+#endif
