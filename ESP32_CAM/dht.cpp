9f27e1ee53b2508e69e606713d0f5bc8dd69fafe
kartvy33 <kartvyrathod33@gmail.com>
2026-07-24 21:34:49 +0530

    Create dht.cpp

---
+++ ESP32_CAM/dht.cpp
@@ -0,0 +1,45 @@
+#include "dht.h"
+
+#include <DHT.h>
+
+#define DHT_PIN 2
+#define DHT_TYPE DHT11
+
+DHT dht(DHT_PIN,DHT_TYPE);
+
+static float temperature=0;
+static float humidity=0;
+
+static unsigned long previousRead=0;
+
+void dhtBegin()
+{
+    dht.begin();
+}
+
+void dhtUpdate()
+{
+    if(millis()-previousRead<2000)
+        return;
+
+    previousRead=millis();
+
+    float t=dht.readTemperature();
+    float h=dht.readHumidity();
+
+    if(!isnan(t))
+        temperature=t;
+
+    if(!isnan(h))
+        humidity=h;
+}
+
+float getTemperature()
+{
+    return temperature;
+}
+
+float getHumidity()
+{
+    return humidity;
+}
