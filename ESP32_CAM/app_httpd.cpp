a597caaf707f85e7152156445ffa09ad74d7c913
kartvy33 <kartvyrathod33@gmail.com>
2026-07-25 09:26:03 +0530

    Update app_httpd.cpp

--- ESP32_CAM/app_httpd.cpp
+++ ESP32_CAM/app_httpd.cpp
@@ -19,6 +19,9 @@
 #include "esp32-hal-ledc.h"
 #include "sdkconfig.h"
 #include "camera_index.h"
+#include "command.h"
+#include "rover_status.h"
+#include "json.h"
 
 #if defined(ARDUINO_ARCH_ESP32) && defined(CONFIG_ARDUHAL_ESP_LOG)
 #include "esp32-hal-log.h"
