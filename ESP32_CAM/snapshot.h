470c73d8fbefd8163d59864a256aadc7a39cfc9f
kartvy33 <kartvyrathod33@gmail.com>
2026-07-25 09:28:56 +0530

    Update snapshot.h

--- ESP32_CAM/snapshot.h
+++ ESP32_CAM/snapshot.h
@@ -2,10 +2,11 @@
 #define SNAPSHOT_H
 
 #include <Arduino.h>
+#include "esp_camera.h"
 
 bool capturePhoto();
 
-camera_fb_t *captureFrame();
+camera_fb_t* captureFrame();
 
 void releaseFrame(camera_fb_t *fb);
 
