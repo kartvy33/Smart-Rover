f6918d5908408ec58c6f75bf5f893caedba6c1a8
kartvy33 <kartvyrathod33@gmail.com>
2026-07-25 08:42:48 +0530

    Create json.cpp

---
+++ ESP32_CAM/json.cpp
@@ -0,0 +1,48 @@
+#include "rover_status.h"
+
+String makeStatusJSON()
+{
+    String json = "{";
+
+    json += "\"temperature\":";
+    json += String(roverStatus.temperature,1);
+
+    json += ",";
+
+    json += "\"humidity\":";
+    json += String(roverStatus.humidity,1);
+
+    json += ",";
+
+    json += "\"battery\":";
+    json += String(roverStatus.battery);
+
+    json += ",";
+
+    json += "\"rain\":";
+    json += roverStatus.rain ? "true" : "false";
+
+    json += ",";
+
+    json += "\"gps\":";
+    json += roverStatus.gps ? "true" : "false";
+
+    json += ",";
+
+    json += "\"radio\":";
+    json += roverStatus.radio ? "true" : "false";
+
+    json += ",";
+
+    json += "\"servoX\":";
+    json += String(roverStatus.servoX);
+
+    json += ",";
+
+    json += "\"servoY\":";
+    json += String(roverStatus.servoY);
+
+    json += "}";
+
+    return json;
+}
