b9f791c9a58f28aedacf3fdb619d73c5d66cac85
kartvy33 <kartvyrathod33@gmail.com>
2026-08-10 19:31:00 +0530

    Update config.h

--- ESP32_CAM/config.h
+++ ESP32_CAM/config.h
@@ -14,8 +14,8 @@
  WiFi
 ==============================*/
 
-#define WIFI_SSID "YOUR_WIFI_NAME"
-#define WIFI_PASSWORD "YOUR_WIFI_PASSWORD"
+#define WIFI_SSID       "Smart-Rover"
+#define WIFI_PASSWORD   "12345678"
 
 /*==============================
  Camera (AI Thinker)
