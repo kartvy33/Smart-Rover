a6c376e6c8bffa44ae93f606fca210057037fc2a
kartvy33 <kartvyrathod33@gmail.com>
2026-08-15 18:30:54 +0530

    Update wifi_manager.h

--- ESP32_CAM/wifi_manager.h
+++ ESP32_CAM/wifi_manager.h
@@ -1,6 +1,20 @@
 #ifndef WIFI_MANAGER_H
 #define WIFI_MANAGER_H
 
-void wifiBegin();
+#include <Arduino.h>
+#include <WiFi.h>
+
+#define WIFI_SSID     "Smart-Rover"
+#define WIFI_PASSWORD "12345678"
+
+#define ROVER_IP      "192.168.4.1"
+
+void wifiManagerBegin();
+
+bool wifiManagerConnected();
+
+IPAddress wifiManagerGetIP();
+
+IPAddress wifiManagerGetRoverIP();
 
 #endif
