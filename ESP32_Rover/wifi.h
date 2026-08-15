f25114931a0ecece8340c66d763fc7f6a31503af
kartvy33 <kartvyrathod33@gmail.com>
2026-08-15 20:59:06 +0530

    Update wifi.h

--- ESP32_Rover/wifi.h
+++ ESP32_Rover/wifi.h
@@ -2,23 +2,8 @@
 #define WIFI_H
 
 #include <Arduino.h>
-#include <WiFi.h>
-
-// ==============================
-// SMART ROVER Wi-Fi
-// ==============================
-
-#define WIFI_SSID     "Smart-Rover"
-#define WIFI_PASSWORD "12345678"
-
-// Fixed Rover Master IP
-#define ROVER_IP_1 192
-#define ROVER_IP_2 168
-#define ROVER_IP_3 4
-#define ROVER_IP_4 1
 
 void wifiBegin();
-
-IPAddress getRoverIP();
+void wifiUpdate();
 
 #endif
